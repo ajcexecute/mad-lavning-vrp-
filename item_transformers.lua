d4b77d4e993abcc86f5a91a3cd4942f80b34f343
ajcexecute <38072067+ajcexecute@users.noreply.github.com>
2019-10-23 10:57:44 +0200

    Create item_transformers.lua

---
+++ item_transformers.lua
@@ -0,0 +1,333 @@
+    {
+    name="køkken", -- menu name
+    permissions = {}, -- you can add permissions
+    r=0,g=125,b=255, -- color
+    max_units=1000,
+    units_per_minute=1000,
+    x=-30.91600227356,y=-587.87414550781,z=88.712272644043,
+    radius=3, height=1.5, -- area
+    recipes = {
+      ["lav scrambled æg med bacon"] = { -- action name
+        description="lav scrambled æg med bacon", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["æg"] = 1,  
+		  ["bacon"] = 1   
+		}, -- items taken per unit
+        products={
+		  ["ægbac"] = 1
+		} -- items given per unit
+      },
+      ["jordbær saft"] = { -- action name "", 
+        description="jordbær saft", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["jsaft"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["jsaftevand"] = 1
+		} -- items given per unit
+      },
+      ["frugt te"] = { -- action name "", 
+        description="frugt te", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["bfrugtte"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["frugtte"] = 1
+		} -- items given per unit
+      },
+      ["lakrids te"] = { -- action name "", 
+        description="lakrids te", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["blakrids"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["lakridste"] = 1
+		} -- items given per unit
+      },
+      ["varm cacao"] = { -- action name "", 
+        description="varm cacao", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["cacaopulver"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["cacao"] = 1
+		} -- items given per unit
+      },
+     ["kaffe"] = { -- action name "", 
+        description="kaffe", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["kaffepulver"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["coffee"] = 1
+		} -- items given per unit
+      },
+     ["lav salat med hvidløgs dressing"] = { -- action name "", 
+        description="lav salat med hvidløgs dressing", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["agurk"] = 1,
+		  ["tomat"] = 1,
+		  ["hvidløg"] = 1,  
+		  ["salathoved"] = 1, 
+		  ["rødpepper"] = 1, 
+		  ["dhvid"] = 1
+		}, -- items taken per unit
+        products={
+		  ["salat1"] = 1
+		} -- items given per unit
+      },
+     ["Lav lasagne"] = { -- action name "", 
+        description="Lav lasagne", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["lasangeplader"] = 1,
+		  ["tomat"] = 1,
+		  ["hvidløg"] = 1,  
+		  ["hokse"] = 1, 
+		  ["løg"] = 1, 
+		  ["ost"] = 1,
+		  ["salt"] = 1,
+		  ["pepper"] = 1
+		}, -- items taken per unit
+        products={
+		  ["lasange"] = 1
+		} -- items given per unit
+      },
+     ["Lav flæskesteg med kartofler og sovs"] = { -- action name "", 
+        description="Lav flæskesteg med kartofler og sovs", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["råflæskesteg"] = 1,
+		  ["milk"] = 1,
+		  ["vand"] = 1,  
+		  ["mel"] = 1, 
+		  ["sovsekulør"] = 1, 
+		  ["kartofler"] = 1,
+		  ["salt"] = 1,
+		  ["pepper"] = 1
+		}, -- items taken per unit
+        products={
+		  ["flæskesteg"] = 1
+		} -- items given per unit
+      },	  
+      ["kamillete"] = { -- action name
+        description="laver kamillete", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["bkamillete"] = 1, 
+		  ["vand"] =1
+		}, -- items taken per unit
+        products={
+		  ["kamillete"] = 1
+		} -- items given per unit
+      }
+    }
+  },
+    {
+    name="køkken", -- menu name
+    permissions = {}, -- you can add permissions
+    r=0,g=125,b=255, -- color
+    max_units=1000,
+    units_per_minute=1000,
+    x=-31.173377990723,y=-589.02124023438,z=88.712249755859,
+    radius=3, height=1.5, -- area
+    recipes = {
+      ["lav scrambled æg med bacon"] = { -- action name
+        description="lav scrambled æg med bacon", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["æg"] = 1,  
+		  ["bacon"] = 1   
+		}, -- items taken per unit
+        products={
+		  ["ægbac"] = 1
+		} -- items given per unit
+      },
+      ["jordbær saft"] = { -- action name "", 
+        description="jordbær saft", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["jsaft"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["jsaftevand"] = 1
+		} -- items given per unit
+      },
+      ["frugt te"] = { -- action name "", 
+        description="frugt te", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["bfrugtte"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["frugtte"] = 1
+		} -- items given per unit
+      },
+      ["lakrids te"] = { -- action name "", 
+        description="lakrids te", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["blakrids"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["lakridste"] = 1
+		} -- items given per unit
+      },
+      ["varm cacao"] = { -- action name "", 
+        description="varm cacao", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["cacaopulver"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["cacao"] = 1
+		} -- items given per unit
+      },
+     ["kaffe"] = { -- action name "", 
+        description="kaffe", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["kaffepulver"] = 1,
+		  ["vand"] = 1
+		}, -- items taken per unit
+        products={
+		  ["coffee"] = 1
+		} -- items given per unit
+      },
+     ["lav salat med hvidløgs dressing"] = { -- action name "", 
+        description="lav salat med hvidløgs dressing", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["agurk"] = 1,
+		  ["tomat"] = 1,
+		  ["hvidløg"] = 1,  
+		  ["salathoved"] = 1, 
+		  ["rødpepper"] = 1, 
+		  ["dhvid"] = 1
+		}, -- items taken per unit
+        products={
+		  ["salat1"] = 1
+		} -- items given per unit
+      },
+     ["Lav lasagne"] = { -- action name "", 
+        description="Lav lasagne", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["lasangeplader"] = 1,
+		  ["tomat"] = 1,
+		  ["hvidløg"] = 1,  
+		  ["hokse"] = 1, 
+		  ["løg"] = 1, 
+		  ["ost"] = 1,
+		  ["salt"] = 1,
+		  ["pepper"] = 1
+		}, -- items taken per unit
+        products={
+		  ["lasange"] = 1
+		} -- items given per unit
+      },
+     ["Lav flæskesteg med kartofler og sovs"] = { -- action name "", 
+        description="Lav flæskesteg med kartofler og sovs", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["råflæskesteg"] = 1,
+		  ["milk"] = 1,
+		  ["vand"] = 1,  
+		  ["mel"] = 1, 
+		  ["sovsekulør"] = 1, 
+		  ["kartofler"] = 1,
+		  ["salt"] = 1,
+		  ["pepper"] = 1
+		}, -- items taken per unit
+        products={
+		  ["flæskesteg"] = 1
+		} -- items given per unit
+      },	  
+      ["kamillete"] = { -- action name
+        description="laver kamillete", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={
+		  ["bkamillete"] = 1, 
+		  ["vand"] =1
+		}, -- items taken per unit
+        products={
+		  ["kamillete"] = 1
+		} -- items given per unit
+      }
+    }
+  },   
+
+-- potitioner
+  {"kitchen", -1153.3295898438,-1521.01953125,10.632727622986},
+  {"kitchen", 343.90447998047,-1002.8638305664,-99.196266174316},
+  {"kitchen", 344.05358886719,-1002.8227539063,-99.196250915527},
+  {"kitchen", 344.0576171875,-1002.6569213867,-99.196258544922},
+  {"kitchen", -10.211909294128,-1428.5477294922,31.101476669312},
+  {"kitchen", 121.88366699219,554.94085693359,184.29707336426},
+  {"kitchen", -569.19134521484,655.74084472656,145.83207702637},
+  {"kitchen", 339.75924682617,430.88421630859,149.38069152832},
+  {"kitchen", 121.92737579346,554.95086669922,184.29707336426},
+  {"kitchen", 1393.4619140625,1145.3422851563,114.33359527588},
+  {"kitchen", -769.12200927734,328.4921875,221.85513305664},
+  {"kitchen", -786.90563964844,328.79388427734,206.21838378906},
+  {"kitchen", -787.31781005859,329.09390258789,158.59901428223},
+  {"kitchen", -609.2216796875,53.712383270264,106.62445831299},
+  {"kitchen", -1459.1501464844,-535.04058837891,68.154052734375},
+  {"kitchen", -1458.9887695313,-535.10357666016,55.526393890381},
+  {"kitchen", -917.48370361328,-380.10894775391,108.03771972656},
+  {"kitchen", -912.61431884766,-370.95669555664,84.077903747559},
+  {"kitchen", -470.19195556641,-695.99536132813,75.68433380127},
+  {"kitchen", -465.89984130859,-702.24163818359,52.003200531006},
+  {"kitchen", -770.25958251953,338.73275756836,211.3970489502},
+  {"kitchen", -617.91412353516,44.163917541504,97.600036621094},
+  {"kitchen", -1473.1860351563,-536.95184326172,73.444145202637},
+  {"kitchen", -918.93499755859,-385.1926574707,113.67457580566},
+  {"kitchen", 658.73443603516,-716.89056396484,37.378147125244},
+  {"kitchen", 76.372291564941,89.744300842285,79.219207763672},
+  {"kitchen", -1339.244140625,-753.61248779297,30.175470352173},
+  {"kitchen", -8.4742126464844,519.07568359375,174.62814331055},
+  {"kitchen", -1432.6004638672,649.59027099609,197.09944152832},
+  {"kitchen", -2521.5183105469,753.65600585938,303.39929199219},
+  {"kitchen", -2520.3952636719,741.57971191406,307.85641479492},
+  {"kitchen", 270.35095214844,-1113.5843505859,77.266738891602},
+  {"kitchen", -921.78979492188,-1314.4342041016,9.9399948120117},
+  {"kitchen", -778.64147949219,328.08898925781,196.08584594727},
+  {"kitchen", -782.47601318359,329.01455688477,187.31314086914},
+  {"kitchen", -782.44665527344,329.0280456543,217.03804016113},
+  {"kitchen", -799.56463623047,186.56507873535,72.605522155762}
